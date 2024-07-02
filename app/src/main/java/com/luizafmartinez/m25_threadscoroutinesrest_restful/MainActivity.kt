b60334293d5b8003425d8973495d98a8701be72d
alxmart <alxmartinez@gmail.com>
2024-07-02 17:07:01 -0300

    Using Query (method recuperarComentariosParaPostagemQuery)

--- app/src/main/java/com/luizafmartinez/m25_threadscoroutinesrest_restful/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m25_threadscoroutinesrest_restful/MainActivity.kt
@@ -186,7 +186,13 @@ class MainActivity : AppCompatActivity() {
 
         try {// Passa a Interface e cria objeto (enderecoAPI)
             val postagemAPI = retrofit.create(PostagemAPI::class.java)
-            retorno = postagemAPI.recuperarComentariosParaPostagem(1) //Método dentro da Interface PostagemAPI
+
+            // Path
+            //retorno = postagemAPI.recuperarComentariosParaPostagem(1)
+
+            // Query
+            retorno = postagemAPI.recuperarComentariosParaPostagemQuery(1)
+
         } catch (e: Exception) {
             e.printStackTrace()
             Log.i("info_jsonplace", "Erro ao recuperar.")
