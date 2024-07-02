45ef1d95aae2315d93f09d0495698f4ff997e2e9
alxmart <alxmartinez@gmail.com>
2024-07-02 17:02:14 -0300

    Added suspend fun recuperarComentariosParaPostagemQuery

--- app/src/main/java/com/luizafmartinez/m25_threadscoroutinesrest_restful/api/PostagemAPI.kt
+++ app/src/main/java/com/luizafmartinez/m25_threadscoroutinesrest_restful/api/PostagemAPI.kt
@@ -5,6 +5,7 @@ import com.luizafmartinez.m25_threadscoroutinesrest_restful.api.model.Postagem
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PostagemAPI {
     //https://jsonplaceholder.typicode.com/ + posts
@@ -19,6 +20,11 @@ interface PostagemAPI {
     @GET("posts/{id}/comments")
     suspend fun recuperarComentariosParaPostagem(
         @Path("id")  id: Int
+    ) : Response<List<Comentario>>
+
+    @GET("comments")   //comments?postId=1&idcomentario=2&
+    suspend fun recuperarComentariosParaPostagemQuery(
+        @Query("postId")  id: Int
     ) : Response<List<Comentario>>
 
 }
