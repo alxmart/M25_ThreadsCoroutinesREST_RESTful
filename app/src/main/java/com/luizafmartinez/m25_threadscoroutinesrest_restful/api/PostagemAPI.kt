09ee6c7339fc0cc36c7f095de2d838d62926f7af
alxmart <alxmartinez@gmail.com>
2024-07-02 15:28:34 -0300

    Creating suspend fun recuperarPostagemUnica

--- app/src/main/java/com/luizafmartinez/m25_threadscoroutinesrest_restful/api/PostagemAPI.kt
+++ app/src/main/java/com/luizafmartinez/m25_threadscoroutinesrest_restful/api/PostagemAPI.kt
@@ -3,10 +3,16 @@ package com.luizafmartinez.m25_threadscoroutinesrest_restful.api
 import com.luizafmartinez.m25_threadscoroutinesrest_restful.api.model.Postagem
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PostagemAPI {
     //https://jsonplaceholder.typicode.com/ + posts
     @GET("posts")
     suspend fun recuperarPostagens() : Response<List<Postagem>>
+
+    @GET("posts/{id}")
+    suspend fun recuperarPostagemUnica(
+        @Path("id")  id: Int
+    ) : Response<Postagem>
 
 }
