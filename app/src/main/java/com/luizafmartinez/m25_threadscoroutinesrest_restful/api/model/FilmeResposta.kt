2bb102ae8d0353c24534441c6a81caa09ebc9d89
alxmart <alxmartinez@gmail.com>
2024-07-04 16:27:08 -0300

    Created FilmeResposta.kt using Kotlin data class file to Json / Rename Response.kt to Filme.kt

---
+++ app/src/main/java/com/luizafmartinez/m25_threadscoroutinesrest_restful/api/model/FilmeResposta.kt
@@ -0,0 +1,8 @@
+package com.luizafmartinez.m25_threadscoroutinesrest_restful.api.model
+
+data class FilmeResposta(
+    val page: Int,
+    val results: List<Filme>,
+    val total_pages: Int,
+    val total_results: Int
+)
